2c3188e532e31e05a57211e0a5fc8ba579f4710f
wldn-di <springbom0214@gmail.com>
2025-06-24 17:48:04 +0900

    account -ing (클래스 분리중)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import account.Account;
+
+public class Main {
+    public static void main(String[] args) {
+       account3();
+    }
+
+    public static void account3(){
+        Account account = new Account();
+        account.list();
+    }
+}
